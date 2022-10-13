f3ead0447ef369e0ea6f3321d047ff07a5cfa02d
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-10-13 13:48:03 -0700

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int? Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+        public Car() { }
+        public Car(int? year, string make, string model, string engineNoise, string honkNoise, bool IsDrivable = true)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+        }
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"Engine Noise: {EngineNoise}");
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"Honk Noise: {HonkNoise}");
+        }
+
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,7 +6,9 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
+            //Instantiate the Carlot at the beginning of the program and as you create a car add the car to the list.
+
+            var lot = new CarLot();
 
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
@@ -16,7 +18,51 @@ namespace CarLotSimulator
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
+
+            Car car1 = new Car();
+            car1.Year = 2018;
+            car1.Make = "Tesla";
+            car1.Model = "X";
+            car1.EngineNoise = "None";
+            car1.HonkNoise = "Honk";
+            car1.IsDrivable = true;
+            lot.Cars.Add(car1);
+
+            Car car2 = new Car()
+            {
+                Year = 2018,
+                Make = "Tesla",
+                Model = "3",
+                EngineNoise = "Nada",
+                HonkNoise = "Beep",
+                IsDrivable = true
+            };
+            lot.Cars.Add(car2);
+
+            Car car3 = new Car(2023, "Tesla", "Cybertruck", "Zero", "BOOM!", true);
+            lot.Cars.Add(car3);
+
             //Call each of the methods for each car
+            Console.WriteLine($"{car1.Year} {car1.Make} {car1.Model}");
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+            Console.WriteLine($"--------------------");
+            Console.WriteLine($"{car2.Year} {car2.Make} {car2.Model}");
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+            Console.WriteLine($"--------------------");
+            Console.WriteLine($"{car3.Year} {car3.Make} {car3.Model}");
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
+            Console.WriteLine($"--------------------");
+
+            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            foreach (var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year}\nMake: {car.Make}\nModel: {car.Model}");
+                Console.WriteLine($"--------------------");
+            }
 
             //*************BONUS*************//
 
@@ -26,8 +72,6 @@ namespace CarLotSimulator
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
     }
 }
